fd750adb304e7d10db59753b7b352a62a01c9f81
Anton Isakov <anton.isakov@stud.techin.lt>
2024-06-12 17:54:51 +0300

    project created

---
+++ src/main/java/com/example/BookRecommendationApp/BookRecommendationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookRecommendationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommendationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommendationAppApplication.class, args);
+	}
+
+}
